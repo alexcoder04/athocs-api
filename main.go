0ccca6d672b077cbffd4f60102e8480565bd3f3e
alexcoder04 <alexcoder04@protonmail.com>
2025-02-09 12:04:59 +0100

    add debug mode which allows all origins in cors

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"path/filepath"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/gofiber/fiber/v3/middleware/static"
 )
 
 func main() {
+	debug := flag.Bool("debug", false, "enable cors for all origins")
+	flag.Parse()
+
 	app := fiber.New()
+
+	if *debug {
+		// allowing all origins is "bad practice" and disallowed in fiber
+		// we dont care and use AllowOriginsFunc to bypass that restriction
+		app.Use(cors.New(cors.Config{
+			AllowOriginsFunc: func(origin string) bool {
+				return true
+			},
+		}))
+	}
 
 	// api
 	app.Get("/api/data", DataHandler)
